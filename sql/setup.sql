5c04381aa8e8114cc2e83a9cb0dbf82d4b5e37b8
warrioryoko <miangpellegri@gmail.com>
2020-09-14 17:49:22 -0700

    Models and Store function works, along with passing tests

---
+++ sql/setup.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS books;
+
+CREATE TABLE books (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title TEXT NOT NULL,
+  cover_image TEXT NOT NULL,
+  rating TEXT NOT NULL,
+  price TEXT NOT NULL,
+  in_stock BOOL NOT NULL
+);
